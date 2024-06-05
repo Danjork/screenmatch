21d17ce8d0cb3885102789afee31faa7b857ca43
jaraujo <115090337+jaraujomontesa@users.noreply.github.com>
2024-06-05 17:20:00 -0400

    Datos Episodio

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,9 @@
 package com.aluracursos.screenmatch;
 
+import com.aluracursos.screenmatch.model.DatosEpisodio;
+import com.aluracursos.screenmatch.model.DatosSerie;
 import com.aluracursos.screenmatch.service.ConsumoAPI;
+import com.aluracursos.screenmatch.service.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,6 +22,14 @@ public class ScreenmatchApplication  implements CommandLineRunner {
 		var json =  consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&apikey=219096d3");
 
 		System.out.println(json);
+
+		ConvierteDatos conversor = new ConvierteDatos();
+		var datos = conversor.obtenerDatos(json, DatosSerie.class);
+		System.out.println(datos);
+
+		json = consumoApi.obtenerDatos("https://www.omdbapi.com/?t=game+of+thrones&Season=1&episode=1&apikey=219096d3");
+		DatosEpisodio episodio =  conversor.obtenerDatos(json, DatosEpisodio.class);
+		System.out.println(episodio);
 
 	}
 }
---
+++ src/main/java/com/aluracursos/screenmatch/model/DatosEpisodio.java
@@ -0,0 +1,13 @@
+package com.aluracursos.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//se usa para obviar datos, en este caso el API contiene 20 columnas y con esto solo traemos 3 y no errojara error
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosEpisodio(
+        @JsonAlias("Title") String titulo,
+        @JsonAlias("Episode") Integer numeroEpisodio,
+        @JsonAlias("imdbRating") String evaluacion,
+        @JsonAlias("Released") String fechaDeLanzamiento
+) {
+}
